4e5b8deab7f6cf82f96b9dc3257c43ef31ca3f8c
Tirgei <tirgeic@gmail.com>
2018-07-07 18:02:41 +0300

    converted to DataBinding

---
+++ app/src/main/java/com/gelostech/automart/callbacks/CarCallback.kt
@@ -0,0 +1,12 @@
+package com.gelostech.automart.callbacks
+
+import android.view.View
+import com.gelostech.automart.models.Car
+
+interface CarCallback {
+
+    fun onClick(v: View)
+
+    fun onClick(v: View, car: Car)
+
+}
