3011da4ee9f491682770c6a1ebfd3cebe4dc48c5
Tirgei <tirgeic@gmail.com>
2018-07-07 23:59:58 +0300

    created my uploads UI

---
+++ app/src/main/java/com/gelostech/automart/activities/MyUploadsActivity.kt
@@ -0,0 +1,82 @@
+package com.gelostech.automart.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.view.MenuItem
+import com.gelostech.automart.R
+import com.gelostech.automart.commoners.AppUtils
+import com.gelostech.automart.commoners.BaseActivity
+import com.gelostech.automart.fragments.MyCarsFragment
+import com.gelostech.automart.fragments.MyPartsFragment
+import com.gelostech.automart.utils.PagerAdapter
+import kotlinx.android.synthetic.main.activity_my_uploads.*
+
+class MyUploadsActivity : BaseActivity(), TabLayout.OnTabSelectedListener {
+
+    companion object {
+        private const val CARS = "CARS"
+        private const val PARTS = "PARTS"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_uploads)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "My Uploads"
+
+        setupViewPager()
+        setupTabs()
+    }
+
+    private fun setupViewPager() {
+        val adapter = PagerAdapter(supportFragmentManager, this)
+        val cars = MyCarsFragment()
+        val parts = MyPartsFragment()
+
+        adapter.addAllFrags(cars, parts)
+        adapter.addAllTitles(CARS, PARTS)
+
+        viewpager.adapter = adapter
+        viewpager.offscreenPageLimit = 1
+        viewpager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tabs))
+
+    }
+
+    private fun setupTabs() {
+        tabs.setupWithViewPager(viewpager)
+        tabs.addOnTabSelectedListener(this)
+    }
+
+    override fun onTabReselected(tab: TabLayout.Tab?) {
+
+    }
+
+    override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+    }
+
+    override fun onTabSelected(tab: TabLayout.Tab?) {
+        viewpager.setCurrentItem(tab!!.position, true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            android.R.id.home -> onBackPressed()
+        }
+
+        return true
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        AppUtils.animateEnterLeft(this)
+    }
+}
