17eab6bd8086569189a839a986dec03515e448bf
Tirgei <tirgeic@gmail.com>
2018-07-08 10:25:10 +0300

    added internet connectivity checker

--- app/src/main/java/com/gelostech/automart/commoners/BaseActivity.kt
+++ app/src/main/java/com/gelostech/automart/commoners/BaseActivity.kt
@@ -2,10 +2,15 @@ package com.gelostech.automart.commoners
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.gelostech.automart.utils.Connectivity
 
 open class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    fun isConnected(): Boolean {
+        return Connectivity.isConnected(this)
     }
 }
