09817c3bdcc965991494d9c20e3a91cf7ae42847
Vincent Tirgei <tirgeic@gmail.com>
2018-07-16 12:37:37 +0300

    fixed loading features

---
+++ app/src/main/java/com/gelostech/automart/adapters/FeaturesAdapter.kt
@@ -0,0 +1,50 @@
+package com.gelostech.automart.adapters
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import com.gelostech.automart.R
+import com.gelostech.automart.commoners.AppUtils.setDrawable
+import com.gelostech.automart.databinding.ItemFeaturesBinding
+import com.gelostech.automart.utils.inflate
+import com.gelostech.automart.utils.setDrawable
+import com.mikepenz.ionicons_typeface_library.Ionicons
+import timber.log.Timber
+
+class FeaturesAdapter(val context: Context) : RecyclerView.Adapter<FeaturesAdapter.FeaturesHolder>() {
+        val features = mutableListOf<String>()
+
+        fun addFeatures(features: MutableList<String>) {
+            this.features.addAll(features)
+            notifyDataSetChanged()
+            Timber.e("Added details")
+        }
+
+        fun features(): List<String> {
+            return features
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FeaturesHolder {
+            return FeaturesHolder(parent.inflate(R.layout.item_features), context)
+        }
+
+        override fun getItemCount(): Int = features.size
+
+        override fun onBindViewHolder(holder: FeaturesHolder, position: Int) {
+            holder.bind(features[position])
+        }
+
+        class FeaturesHolder(private val binding: ItemFeaturesBinding, val context: Context) : RecyclerView.ViewHolder(binding.root) {
+
+            init {
+                binding.tv.setDrawable(setDrawable(context, Ionicons.Icon.ion_android_checkmark_circle, R.color.colorAccent, 18))
+            }
+
+            fun bind(feature: String) {
+
+                binding.feature = feature
+            }
+
+        }
+
+    }
