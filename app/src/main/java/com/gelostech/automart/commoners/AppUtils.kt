b1732ee37fd5c4715b5d16c2609d73966a766c9c
Vincent Tirgei <tirgeic@gmail.com>
2018-07-18 15:49:04 +0300

    added function to validate input fields

--- app/src/main/java/com/gelostech/automart/commoners/AppUtils.kt
+++ app/src/main/java/com/gelostech/automart/commoners/AppUtils.kt
@@ -2,8 +2,14 @@ package com.gelostech.automart.commoners
 
 import android.app.Activity
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.support.v4.content.ContextCompat
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
 import com.gelostech.automart.R
 import com.mikepenz.iconics.IconicsDrawable
 import com.mikepenz.iconics.typeface.IIcon
@@ -32,6 +38,46 @@ object AppUtils {
      */
     fun setDrawable(context: Context, icon: IIcon, color: Int, size: Int): Drawable {
         return IconicsDrawable(context).icon(icon).color(ContextCompat.getColor(context, color)).sizeDp(size)
+    }
+
+
+    /**
+     *  This function returns a drawable from a Bitmap
+     */
+    fun drawableToBitmap(drawable: Drawable): Bitmap {
+        if (drawable is BitmapDrawable) {
+            if (drawable.bitmap != null) {
+                return drawable.bitmap
+            }
+        }
+
+        val bitmap: Bitmap = if (drawable.intrinsicWidth <= 0 || drawable.intrinsicHeight <= 0) {
+            Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888) // Single color bitmap will be created of 1x1 pixel
+        } else {
+            Bitmap.createBitmap(drawable.intrinsicWidth, drawable.intrinsicHeight, Bitmap.Config.ARGB_8888)
+        }
+
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+        return bitmap
+    }
+
+    fun getColor(context: Context, color: Int): Int {
+        return ContextCompat.getColor(context, color)
+    }
+
+    fun validated(vararg views: View): Boolean {
+        var ok = true
+        for (v in views) {
+            if (v is EditText) {
+                if (TextUtils.isEmpty(v.text.toString())) {
+                    ok = false
+                    v.error = "Required"
+                }
+            }
+        }
+        return ok
     }
 
 }
