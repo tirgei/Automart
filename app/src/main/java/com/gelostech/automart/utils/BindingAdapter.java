a6875a4130e35ab0c040d9fdf22d7a8bd51c0d8e
Vincent Tirgei <tirgeic@gmail.com>
2018-07-23 00:29:43 +0300

    added binding function to load images from Uri

--- app/src/main/java/com/gelostech/automart/utils/BindingAdapter.java
+++ app/src/main/java/com/gelostech/automart/utils/BindingAdapter.java
@@ -1,5 +1,7 @@
 package com.gelostech.automart.utils;
 
+import android.media.Image;
+import android.net.Uri;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -9,10 +11,26 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class BindingAdapter {
 
-    @android.databinding.BindingAdapter({"bind:imageUrl"})
-    public static void loadImage(ImageView view, int url) {
+    @android.databinding.BindingAdapter({"imageUrl"})
+    public static void loadImageFromInt(ImageView view, int url) {
         Glide.with(view.getContext())
                 .load(url)
+                .thumbnail(0.05f)
+                .into(view);
+    }
+
+    @android.databinding.BindingAdapter({"imageUrl"})
+    public static void loadImageFromString(ImageView view, String url) {
+        Glide.with(view.getContext())
+                .load(url)
+                .thumbnail(0.05f)
+                .into(view);
+    }
+
+    @android.databinding.BindingAdapter({"imageUri"})
+    public static void loadImageFromUri(ImageView view, Uri uri) {
+        Glide.with(view.getContext())
+                .load(uri)
                 .thumbnail(0.05f)
                 .into(view);
     }
