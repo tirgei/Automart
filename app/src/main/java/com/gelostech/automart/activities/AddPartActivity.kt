9bd4b7f7b6e237d8c8ed5b81a76c1c2275a31944
Vincent Tirgei <tirgeic@gmail.com>
2018-07-23 01:02:04 +0300

    added UI for posting part

--- app/src/main/java/com/gelostech/automart/activities/AddPartActivity.kt
+++ app/src/main/java/com/gelostech/automart/activities/AddPartActivity.kt
@@ -2,18 +2,39 @@ package com.gelostech.automart.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.gelostech.automart.R
 import com.gelostech.automart.commoners.AppUtils
+import kotlinx.android.synthetic.main.activity_add_part.*
 
 class AddPartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_part)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Post part"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId) {
+            android.R.id.home -> onBackPressed()
+        }
+
+        return true
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
         AppUtils.animateEnterLeft(this)
+
     }
+
 }
