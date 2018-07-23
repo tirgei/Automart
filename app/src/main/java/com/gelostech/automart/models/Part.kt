0a0b6a6e51e32778f87ea6f30517c5412a8f1bb2
Vincent Tirgei <tirgeic@gmail.com>
2018-07-23 13:57:48 +0300

    added missing attributes

--- app/src/main/java/com/gelostech/automart/models/Part.kt
+++ app/src/main/java/com/gelostech/automart/models/Part.kt
@@ -5,10 +5,18 @@ import java.io.Serializable
 data class Part(
         var id: String? = null,
         var name: String? = null,
+        var number: String? = null,
+        var category: String? = null,
+        var description: String? = null,
         var sellerId: String? = null,
         var sellerName: String? = null,
-        var price: Int? = null,
+        var price: String? = null,
+        var time: Long? = null,
+        var make: String? = null,
+        var model: String? = null,
+        var quantity: String? = null,
         var image: String? = null,
+        var images: MutableMap<String, String> = mutableMapOf(),
         var holderImage: Int? = null
 ): Serializable {
 }
