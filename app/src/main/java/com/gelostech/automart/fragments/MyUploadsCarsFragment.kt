9731c151f11a4076acfb440aae20ca3bfba8e726
Vincent Tirgei <tirgeic@gmail.com>
2018-07-23 13:56:41 +0300

    added function to fetch Cars from Firestore

--- app/src/main/java/com/gelostech/automart/fragments/MyUploadsCarsFragment.kt
+++ app/src/main/java/com/gelostech/automart/fragments/MyUploadsCarsFragment.kt
@@ -13,9 +13,16 @@ import com.gelostech.automart.R
 import com.gelostech.automart.adapters.CarsAdapter
 import com.gelostech.automart.callbacks.CarCallback
 import com.gelostech.automart.commoners.BaseFragment
+import com.gelostech.automart.commoners.K
 import com.gelostech.automart.models.Car
 import com.gelostech.automart.utils.RecyclerFormatter
+import com.gelostech.automart.utils.hideView
+import com.gelostech.automart.utils.showView
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.Query
+import kotlinx.android.synthetic.main.fragment_my_cars.*
 import kotlinx.android.synthetic.main.fragment_my_cars.view.*
+import timber.log.Timber
 
 class MyUploadsCarsFragment : BaseFragment(), CarCallback {
     private lateinit var carsAdapter: CarsAdapter
@@ -28,70 +35,72 @@ class MyUploadsCarsFragment : BaseFragment(), CarCallback {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         initViews(view)
+
+        loadCars()
     }
 
     private fun initViews(v: View) {
-        v.rv.setHasFixedSize(true)
-        v.rv.layoutManager = LinearLayoutManager(activity!!)
-        v.rv.itemAnimator = DefaultItemAnimator()
-        v.rv.addItemDecoration(RecyclerFormatter.DoubleDividerItemDecoration(activity!!))
+        rv.setHasFixedSize(true)
+        rv.layoutManager = LinearLayoutManager(activity!!)
+        rv.itemAnimator = DefaultItemAnimator()
+        rv.addItemDecoration(RecyclerFormatter.DoubleDividerItemDecoration(activity!!))
 
         carsAdapter = CarsAdapter(activity!!, this)
-        v.rv.adapter = carsAdapter
-        v.rv.showShimmerAdapter()
-
-        Handler().postDelayed({
-            v.rv.hideShimmerAdapter()
-            loadSample()
-        }, 2500)
+        rv.adapter = carsAdapter
+        rv.showShimmerAdapter()
     }
 
-    private fun loadSample() {
+    private fun loadCars() {
+        getFirestore().collection(K.CARS)
+                .orderBy(K.TIMESTAMP, Query.Direction.DESCENDING)
+                .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
+                    if (firebaseFirestoreException != null) {
+                        Timber.e("Error fetching cars $firebaseFirestoreException")
+                        noCars()
+                    }
 
-        val car3 = Car()
-        car3.make = "Toyota"
-        car3.model = "Hilux Surf"
-        car3.price = 11250000.toString()
-        car3.holderAvatar = R.drawable.person
-        car3.holderImage = R.drawable.hilux
-        car3.year = 1997.toString()
-        car3.transmission = "Manual"
-        car3.mileage = 189000.toString()
-        car3.location = "Mombasa rd"
-        car3.sellerName = "Skyline Motors"
-        car3.time = System.currentTimeMillis()
-        carsAdapter.addCar(car3)
+                    if (querySnapshot == null || querySnapshot.isEmpty) {
+                        noCars()
+                    } else {
+                        hasCars()
 
-        val car4 = Car()
-        car4.make = "Honda"
-        car4.model = "Fit"
-        car4.price = 68000.toString()
-        car4.holderAvatar = R.drawable.person
-        car4.holderImage = R.drawable.fit
-        car4.year = 2011.toString()
-        car4.transmission = "Automatic"
-        car4.mileage = 75000.toString()
-        car4.location = "Outering road"
-        car4.sellerName = "Skyline Motors"
-        car4.time = System.currentTimeMillis()
-        carsAdapter.addCar(car4)
+                        for (docChange in querySnapshot.documentChanges) {
 
-        val car5 = Car()
-        car5.make = "Toyota"
-        car5.model = "Bb"
-        car5.price = 875000.toString()
-        car5.holderAvatar = R.drawable.person
-        car5.holderImage = R.drawable.bb
-        car5.year = 2009.toString()
-        car5.transmission = "Automatic"
-        car5.mileage = 68000.toString()
-        car5.location = "Ngong road"
-        car5.sellerName = "Agba Auto"
-        car5.time = System.currentTimeMillis()
-        carsAdapter.addCar(car5)
+                            when(docChange.type) {
+                                DocumentChange.Type.ADDED -> {
+                                    val car = docChange.document.toObject(Car::class.java)
+                                    if (car.sellerId == getUid()) {
+                                        carsAdapter.addCar(car)
+                                    }
+                                }
 
+                                DocumentChange.Type.MODIFIED -> {
+
+                                }
+
+                                DocumentChange.Type.REMOVED -> {
+
+                                }
+
+                            }
+
+                        }
+
+                    }
+                }
+    }
+
+    private fun hasCars() {
+        rv.hideShimmerAdapter()
+        empty.hideView()
+        rv.showView()
+    }
+
+    private fun noCars() {
+        rv.hideShimmerAdapter()
+        rv.hideView()
+        empty.showView()
     }
 
     override fun onClick(v: View, car: Car) {
