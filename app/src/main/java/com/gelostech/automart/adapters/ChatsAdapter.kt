d4305734333e4ddb58bd2a23c9a8dd0724a3ebb3
Vincent Tirgei <tirgeic@gmail.com>
2018-07-28 00:14:40 +0300

    renamed  ChatListAdapter

--- app/src/main/java/com/gelostech/automart/adapters/ChatsAdapter.kt
+++ app/src/main/java/com/gelostech/automart/adapters/ChatsAdapter.kt
@@ -1,28 +1,33 @@
 package com.gelostech.automart.adapters
 
 import android.support.v7.widget.RecyclerView
-import android.view.View
 import android.view.ViewGroup
 import com.gelostech.automart.R
 import com.gelostech.automart.callbacks.ChatListCallback
-import com.gelostech.automart.databinding.ItemChatListBinding
-import com.gelostech.automart.models.ChatItem
+import com.gelostech.automart.databinding.ItemChatBinding
+import com.gelostech.automart.models.Chat
 import com.gelostech.automart.utils.TimeFormatter
 import com.gelostech.automart.utils.inflate
-import com.gelostech.automart.utils.loadUrl
-import kotlinx.android.synthetic.main.item_chat_list.view.*
-import java.lang.ref.WeakReference
 
-class ChatListAdapter(private val callback: ChatListCallback) : RecyclerView.Adapter<ChatListAdapter.ChatListHolder>() {
-    private var chats = mutableListOf<ChatItem>()
+class ChatsAdapter(private val callback: ChatListCallback) : RecyclerView.Adapter<ChatsAdapter.ChatListHolder>() {
+    private var chats = mutableListOf<Chat>()
 
-    fun addChat(chat: ChatItem) {
+    fun addChat(chat: Chat) {
         chats.add(chat)
         notifyItemInserted(chats.size - 1)
     }
 
+    fun updateChat(updatedChat: Chat) {
+        for ((index, chat) in chats.withIndex()) {
+            if (updatedChat.id == chat.id) {
+                chats[index] = updatedChat
+                notifyItemChanged(index, updatedChat)
+            }
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatListHolder {
-        return ChatListHolder(parent.inflate(R.layout.item_chat_list), callback)
+        return ChatListHolder(parent.inflate(R.layout.item_chat), callback)
     }
 
     override fun getItemCount(): Int = chats.size
@@ -32,16 +37,16 @@ class ChatListAdapter(private val callback: ChatListCallback) : RecyclerView.Ada
     }
 
     interface OnItemClickListener {
-        fun onItemClickListener(chat: ChatItem)
+        fun onItemClickListener(chat: Chat)
     }
 
-    class ChatListHolder(private val binding: ItemChatListBinding, callback: ChatListCallback) : RecyclerView.ViewHolder(binding.root){
+    class ChatListHolder(private val binding: ItemChatBinding, callback: ChatListCallback) : RecyclerView.ViewHolder(binding.root){
 
         init {
             binding.callback = callback
         }
 
-        fun bindViews(chat: ChatItem) {
+        fun bindViews(chat: Chat) {
             binding.data = chat
             binding.time = TimeFormatter().getChatTimeStamp(chat.time!!)
         }
